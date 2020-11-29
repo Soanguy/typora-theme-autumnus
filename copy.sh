10ee4c97de50bd826bd0e0dbe204f9ca2a0e68de
Liuhao <326047736@qq.com>
2020-11-29 19:56:14 +0800

    patch

---
+++ copy.sh
@@ -0,0 +1,6 @@
+#将相关文件复制到 Typora 主题文件夹（不同电脑需单独设置）。用于调试
+cp -r -f -v autumnus-import C:/Users/liuhao/AppData/Roaming/Typora/themes
+cp -r -f -v autumnus/*css C:/Users/liuhao/AppData/Roaming/Typora/themes
+cp -r -f -v Less-autumnus/*css C:/Users/liuhao/AppData/Roaming/Typora/themes
+cp -r -f -v Opt-autumnus/*css C:/Users/liuhao/AppData/Roaming/Typora/themes
+cp -r -f -v QD-autumnus/*css C:/Users/liuhao/AppData/Roaming/Typora/themes
---
+++ organize.sh
@@ -0,0 +1,7 @@
+#将相关文件复制到 dist 文件夹下。方便打包发布
+mkdir dist
+cp -r -f -v autumnus-import dist
+cp -r -f -v autumnus/*css dist
+cp -r -f -v Less-autumnus/*css dist
+cp -r -f -v Opt-autumnus/*css dist
+cp -r -f -v QD-autumnus/*css dist
