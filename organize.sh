58a377b6c3d038e1613d0a8fce863931d21a3566
soanguy <ai1473722171@gmail.com>
2020-11-29 23:49:49 +0800

    合并请求

--- organize.sh
+++
@@ -1,7 +0,0 @@
-#将相关文件复制到 dist 文件夹下。方便打包发布
-mkdir dist
-cp -r -f -v autumnus-import dist
-cp -r -f -v autumnus/*css dist
-cp -r -f -v Less-autumnus/*css dist
-cp -r -f -v Opt-autumnus/*css dist
-cp -r -f -v QD-autumnus/*css dist
